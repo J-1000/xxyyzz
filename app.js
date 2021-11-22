5bba6767d23dc5b6755152bf7e450ec82543cfd5
Aleksandar Velichkovikj <al.velichkovikj@gmail.com>
2021-11-22 11:42:44 +0100

    added style / script/ index view

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv/config");
+require('dotenv/config')
 
 // ℹ️ Connects to the database
-require("./db");
+require('./db')
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require("express");
+const express = require('express')
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
-const hbs = require("hbs");
+const hbs = require('hbs')
 
-const app = express();
+const app = express()
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app);
+require('./config')(app)
 
-const projectName = "blogify";
-const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
+const projectName = 'blogify'
+const capitalized = (string) =>
+  string[0].toUpperCase() + string.slice(1).toLowerCase()
 
-app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
+app.locals.title = `${capitalized(projectName)} created with IronLauncher`
 
 // 👇 Start handling routes here
-const index = require("./routes/index");
-app.use("/", index);
-
-const authRoutes = require("./routes/auth");
-app.use("/auth", authRoutes);
+const index = require('./routes/index')
+app.use('/', index)
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+require('./error-handling')(app)
 
-module.exports = app;
+module.exports = app
