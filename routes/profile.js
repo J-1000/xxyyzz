3243456abd885aa07575973dd1f19851c5fb8ec3
Thong Nguyen <nguyenthong17@gmail.com>
2021-11-23 10:53:15 +0100

    edit profile done

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const Editor = require('../models/Editor')
-const isLoggedIn = require('../middleware/isLoggedIn')
+const isLoggedIn = require('../middleware/isLoggedIn');
+const bcrypt = require('bcrypt')
+
 
 router.get('/profile', isLoggedIn,(req,res,next) => {
     const id = req.session.user._id;
@@ -21,16 +23,44 @@ router.get('/logout', (req, res, next) => {
     })
 })
 
-router.get('/profile/edit',(req, res, next) => {
+router.get('/profile/edit',isLoggedIn,(req, res, next) => {
     const id = req.session.user._id;
 
     Editor.findById(id)
         .then(foundEditor => {
-            // res.send(foundEditor)
             res.render('editor/edit-profile', foundEditor)
         })
 
 });
+
+router.post('/profile/edit',(req, res, next) => {
+    const id = req.session.user._id;
+    const {bio, email, password} = req.body;
+
+    const salt = bcrypt.genSaltSync()
+    const hash = bcrypt.hashSync(password, salt)
+
+    if (password.length < 8 || password.length === 0) {
+        res.render('editor/edit-profile', {...req.session.user,errorMessage: 'Password must have at least 8 characters'});
+        return
+    }
+
+    if (email.length === 0 || bio.length === 0) { 
+        res.render('editor/edit-profile', {...req.session.user,errorMessage: 'Bio and Email cannot be empty'});
+    }
+    
+    Editor.findByIdAndUpdate(id, {
+        bio: bio,
+        email: email,
+        password: hash
+    },{
+        new:true
+    })
+        .then(updatedEditor => {
+            res.redirect('/profile')
+        })
+});
+
 
 
 module.exports = router;
